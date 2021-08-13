486c8ffd04e56cd62930a7592a2f33e444286a5e
Jeremy Dryden <jeremy.dryden@gmail.com>
2021-08-12 23:33:53 -0400

    first commit, basic deck creation api endpoint

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from "node-fetch";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  createDeck().then((data) => {
+    if (!data) {
+      https: res.status(404).json("Data not found");
+    }
+
+    https: res.status(200).json(data);
+  });
+}
+
+async function createDeck() {
+  const deckUrl = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
+  const response = await fetch(deckUrl);
+  const data = await response.json();
+  return data;
 }
